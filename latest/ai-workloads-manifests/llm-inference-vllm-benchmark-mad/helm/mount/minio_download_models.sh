6a643b45567c6cb09a6635c7fa0f675e201e612f
Jussi Elo <jussi.elo@amd.com>
2025-08-12 11:32:47 +0200

    Removed everything with mike 2.1.3

--- latest/ai-workloads-manifests/llm-inference-vllm-benchmark-mad/helm/mount/minio_download_models.sh
+++
@@ -1,7 +0,0 @@
-# model defined in ENV
-mc cp --recursive minio-host/${BUCKET_MODEL_PATH}/${MAD_MODEL}/ $WORKPATH/models/${MAD_MODEL}/
-
-# models defined in scenarios
-awk -F, '{print $1}' $WORKPATH/mount/scenarios*.csv | sort | uniq | while read MODEL_ID; do
-    mc cp --recursive minio-host/${BUCKET_MODEL_PATH}/${MODEL_ID}/ $WORKPATH/models/${MODEL_ID}/
-done
